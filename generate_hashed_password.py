471857da141991b8402047ab5f2ed94043af2daf
James Shapiro <jamesshapiro@gmail.com>
2021-12-16 14:28:25 -0500

    create password authorizer that retrieves password from DynamoDB

--- generate_hashed_password.py
+++ generate_hashed_password.py
@@ -1,54 +1,15 @@
 import hashlib
 import base64
-#import random
+import random
 
-"""
 def get_salt():
     ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     return ''.join(random.choice(ALPHABET) for i in range(256))
 
-print(b'hello')
-hello_str = 'hello'
-bytes_str = bytes(hello_str, 'utf-8')
-print(bytes_str)
-    
 salt = get_salt()
+#salt = 'SXxjp17IDpQD8ob66c7yp4qIk2FA4Vbid58UVuB1jyOw8z10yNMqyZKk3iu5j0yX1Z2GgnXAocBpLXRegYR6bTRznLiDPBV6bOM255ODsjRzRi4Q8RwEQkSOcv7ymQWlTVvw4nD16zRnhFZ9pFTig7184sbOeQGPlJGkLRG8MfB5OLKJu9pwv0QIj22mCBuDmurMbxoSP0U0fjMBqxSUMqAcokt8s87ro2wpkU2FfqkivPP62t3w0yUqWVzhEAw7'
 print(f'{salt=}')
 m = hashlib.sha256()
 m.update(bytes(salt, 'utf-8'))
-m.update(b'Nobody inspects')
+m.update(b'admin:admin')
 print(str(m.hexdigest()))
-
-import hashlib
-"""
-
-def lambda_handler(event, context):
-    request = event['Records'][0]['cf']['request']
-    headers = request['headers']
-    salt = 'SXxjp17IDpQD8ob66c7yp4qIk2FA4Vbid58UVuB1jyOw8z10yNMqyZKk3iu5j0yX1Z2GgnXAocBpLXRegYR6bTRznLiDPBV6bOM255ODsjRzRi4Q8RwEQkSOcv7ymQWlTVvw4nD16zRnhFZ9pFTig7184sbOeQGPlJGkLRG8MfB5OLKJu9pwv0QIj22mCBuDmurMbxoSP0U0fjMBqxSUMqAcokt8s87ro2wpkU2FfqkivPP62t3w0yUqWVzhEAw7'
-    auth_hash_value = 'ed35d65557a9ac0eafa472ba64bad47866fc102b99d4c33018f76988382fbdf0'
-    m = hashlib.sha256()
-    m.update(bytes(salt, 'utf-8'))
-    if 'authorization' in headers:
-        # auth_string == 'Basic ' + auth_token
-        auth_string = headers['authorization'][0]['value']
-        print(auth_string)
-        auth_token = auth_string[6:]
-        print(auth_token)
-        decoded = base64.b64decode(auth_token)
-        print(type(decoded))
-        print(decoded)
-    m.update(bytes(PASSWORD, 'utf-8'))
-    auth_token = str(m.hexdigest())
-    auth_string = 'Basic ' + auth_token
-    if 'authorization' not in headers or headers['authorization'][0]['value'] != auth_string:
-        return {
-                "status": '401',
-                "statusDescription": 'Unauthorized',
-                "body"             : 'Unauthorized',
-                "headers": {
-                    'www-authenticate': [
-                    {"key": 'WWW-Authenticate', "value":'Basic'}
-                 ]}  
-                }
-    return request  
---
+++ src/distribution/authorizer.py
@@ -0,0 +1,79 @@
+import hashlib
+import base64
+import boto3
+
+def lambda_handler(event, context):
+    session = boto3.Session()
+    iam_client = session.client("iam")
+    sts_client = session.client("sts")
+    ROLE_NAME = sts_client.get_caller_identity()["Arn"].split("/")[-2]
+    ROLE_POLICY = iam_client.get_role_policy(RoleName=ROLE_NAME, PolicyName="root")[
+        "PolicyDocument"
+    ]
+    TABLE_NAME = ''
+    TABLE_REGION = 'us-east-1'
+    for arn in ROLE_POLICY["Statement"][0]["Resource"]:
+        print(f'{arn=}')
+        if arn.startswith("arn:aws:dynamodb:"):
+            arn_parts = arn.split("/")
+            TABLE_NAME = arn_parts[-1]
+            TABLE_REGION = arn_parts[0].split(":")[3]
+    #TODO: delete these
+    print(TABLE_NAME)
+    print(TABLE_REGION)
+    ddb_client = boto3.client('dynamodb', region_name=TABLE_REGION)
+    response = ddb_client.get_item(
+        TableName = 'gratitude-00-JournalEntriesDDBTable-8S218BHVKMYX',
+        Key={
+            'PK1': {
+                'S': 'PASSWORD',
+            },
+            'SK1': {
+                'S': 'PASSWORD',
+            }
+        }
+    )
+    print(response)
+    item = response['Item']
+    salt = item['SALT']['S'] #'5440cWTwnAjWDtjFqqUYQfwyHv2OcXUags8zYfj8XexDYUBiNlm5WXJDDVBI1Xu6l0Czvpjr1beqhIAfPCRGPnPq0bijWBKraOTFXsUc9cAPknguDjA8SNSWMQctS8zGXObNzhg39ztLzwKocK4IiFKCwebBUvjc6LtUxFfgmttu0l5K4iORXhCElhgt4p8m9lPcdkV4th0ohRLAkeklJjnipgsNM2tFw1LqwV9vh2GpDsyl4nkr7nKt1TTDkaVn'
+    print(salt)
+    auth_hash_value = item['HASH']['S']
+    print(auth_hash_value)
+    request = event['Records'][0]['cf']['request']
+    headers = request['headers']
+    #auth_hash_value = '9fb43ed76f1346687484617030cd8f9865df89990b388672b604bdc635f75f74'
+    m = hashlib.sha256()
+    m.update(bytes(salt, 'utf-8'))
+    if 'authorization' in headers:
+        # auth_string == 'Basic ' + auth_token
+        auth_string = headers['authorization'][0]['value']
+        print(auth_string)
+        auth_token = auth_string[6:]
+        print(auth_token)
+        decoded = base64.b64decode(auth_token)
+        print(type(decoded))
+        print(decoded)
+        m.update(decoded)
+        hex_digest = str(m.hexdigest())
+        print(hex_digest)
+        if hex_digest == auth_hash_value:
+            return request
+    if 'authorization' not in headers or headers['authorization'][0]['value'] != auth_string:
+        return {
+                "status": '401',
+                "statusDescription": 'Unauthorized',
+                "body"             : 'Unauthorized',
+                "headers": {
+                    'www-authenticate': [
+                    {"key": 'WWW-Authenticate', "value":'Basic'}
+                 ]}  
+                }
+    return {
+        "status": '401',
+        "statusDescription": 'Unauthorized',
+        "body"             : 'Unauthorized',
+        "headers": {
+            'www-authenticate': [
+                {"key": 'WWW-Authenticate', "value":'Basic'}
+            ]}  
+    }
